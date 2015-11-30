115a3443b0d441d90fbf73a553f3c087b7ea98fa
Sijae Brown <sijaebrown@mwc-102136.dhcp.missouri.edu>
2015-11-30 12:33:40 -0600

    updated the tables for hopefully the final time

--- database/DDL.sql
+++ database/DDL.sql
@@ -15,11 +15,14 @@ CREATE TABLE food_category(
 */
 
 CREATE TABLE transaction(
-  transaction_id long int,
+  transaction_id long int PRIMARY KEY,
   timedate varchar(16), /*e.g.: '12/11/2015 14:15' */
   quantity integer,
   cashier varchar(50) REFERENCES employee(cashier),
-  c_type varchar(10),
+  c_type varchar(10) REFERENCES customer_type(customer_name),
+  o_type varchar(10) REFERENCES operation_type(type),
+  pay_method varchar(10) REFERENCES payment_method(tender_type),
+  type_of_card varchar(16) REFERENCES card_type(card),
   price decimal(5,2),
   total decimal (7,2)
 ) ENGINE = INNODB;
@@ -39,13 +42,14 @@ CREATE TABLE employee(
 ) ENGINE = INNODB;
 
 CREATE TABLE operation_type(
-  operation_type varchar(10) PRIMARY KEY
+  type varchar(10) PRIMARY KEY
 ) ENGINE = INNODB;
 
 CREATE TABLE payment_method(
-  tender_type varchar(10)
+  tender_type varchar(10) PRIMARY KEY,
+  card_title varchar(16) REFERENCES card_type(card)
 ) ENGINE = INNODB;
 
 CREATE TABLE card_type(
-  card_type varchar(16)
+  card varchar(16) PRIMARY KEY
 ) ENGINE = INNODB;
