61363b105e5217ed845aa2453a43a97ec911de4f
Sijae Brown <sijaebrown@Sijaes-MacBook.local>
2015-12-01 00:16:09 -0600

    last time

--- database/DDL.sql
+++ database/DDL.sql
@@ -27,7 +27,7 @@ CREATE TABLE transaction(
   timedate datetime, /*e.g.: '12/11/2015 14:15' */
   c_name varchar(10) REFERENCES customer_type(customer_name) ON DELETE CASCADE,
   cashier varchar(50) REFERENCES employee(cashier) ON DELETE CASCADE,
-  receipt_num varchar(10) REFERENCES payment_method(receipt_num) ON DELETE CASCADE,
+  receipt_num bigint REFERENCES payment_method(receipt_num) ON DELETE CASCADE,
   gratuity decimal(6,2),
   subtotal decimal(6,2),
   discount decimal(6,2),
@@ -57,7 +57,8 @@ CREATE TABLE operation_type(
 
 CREATE TABLE payment_method(
   tender_type varchar(10),
-  receipt_num varchar(20) REFERENCES card_type ON DELETE CASCADE
+  receipt_num varchar(20) REFERENCES card_type,
+  PRIMARY KEY(receipt_num)
 ) ENGINE = INNODB;
 
 CREATE TABLE card_type(
