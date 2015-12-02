05034ec5587073608b87d6243da3aca7761dbc0d
cmartinezvillar <cmartinezvillar90@gmail.com>
2015-12-02 02:36:48 -0600

    latest version of weekofday.php with added graph

--- gh-login/weekofday.php
+++ gh-login/weekofday.php
@@ -6,23 +6,115 @@
 		header("Location: users.php");
 		exit;
 	}
-
-    require_once "db.conf";
+		
+	if(isset($_GET['numberOfWeeks'])) {
+		$limit = $_GET['numberOfWeeks'];
+	} else {
+		$limit = 1;
+	}
+	
+	require_once "db.conf";
     $link = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname) or die("Connect Error".mysqli_error($link));
-    $sql = "SELECT sum(total) FROM transaction
+    
+	$sql = "SELECT total FROM transaction
+  	        WHERE DATE_FORMAT(CAST(timedate AS date), '%W') = 'Monday'
+  	        ORDER BY DATE_FORMAT(CAST(timedate AS date), '%W') = 'Monday'
+  	        LIMIT $limit;";
+    $result = mysqli_query($link,$sql);
+    $Monday = mysqli_fetch_array($result);
+	$count = 0;
+	
+	for ($x = 0; $x <= $limit; $x++) {
+		$count = $count + $Monday[$x];
+	} 
+	$Monday = $count;
+	
+	$sql = "SELECT total FROM transaction
+  	        WHERE DATE_FORMAT(CAST(timedate AS date), '%W') = 'Tuesday'
+  	        ORDER BY DATE_FORMAT(CAST(timedate AS date), '%W') = 'Tuesday'
+  	        LIMIT $limit;";
+    $result = mysqli_query($link,$sql);
+    $Tuesday = mysqli_fetch_array($result);
+	$count = 0;
+	
+	for ($x = 0; $x <= $limit; $x++) {
+		$count = $count + $Tuesday[$x];
+	} 
+	$Tuesday = $count;
+	
+	$sql = "SELECT total FROM transaction
+  	        WHERE DATE_FORMAT(CAST(timedate AS date), '%W') = 'Wednesday'
+  	        ORDER BY DATE_FORMAT(CAST(timedate AS date), '%W') = 'Wednesday'
+  	        LIMIT $limit;";
+    $result = mysqli_query($link,$sql);
+    $Wednesday = mysqli_fetch_array($result);
+	$count = 0;
+	
+	for ($x = 0; $x <= $limit; $x++) {
+		$count = $count + $Wednesday[$x];
+	} 
+	$Wednesday = $count;
+		
+	$sql = "SELECT total FROM transaction
+  	        WHERE DATE_FORMAT(CAST(timedate AS date), '%W') = 'Thursday'
+  	        ORDER BY DATE_FORMAT(CAST(timedate AS date), '%W') = 'Thursday'
+  	        LIMIT $limit;";
+    $result = mysqli_query($link,$sql);
+    $Thursday = mysqli_fetch_array($result);
+	$count = 0;
+	
+	for ($x = 0; $x <= $limit; $x++) {
+		$count = $count + $Thursday[$x];
+	} 
+	$Thursday = $count;
+	
+	$sql = "SELECT total FROM transaction
+  	        WHERE DATE_FORMAT(CAST(timedate AS date), '%W') = 'Friday'
+  	        ORDER BY DATE_FORMAT(CAST(timedate AS date), '%W') = 'Friday'
+  	        LIMIT $limit;";
+    $result = mysqli_query($link,$sql);
+    $Friday = mysqli_fetch_array($result);
+	$count = 0;
+	
+	for ($x = 0; $x <= $limit; $x++) {
+		$count = $count + $Friday[$x];
+	} 
+	$Friday = $count;
+	
+	$sql = "SELECT total FROM transaction
+  	        WHERE DATE_FORMAT(CAST(timedate AS date), '%W') = 'Saturday'
+  	        ORDER BY DATE_FORMAT(CAST(timedate AS date), '%W') = 'Saturday'
+  	        LIMIT $limit;";
+    $result = mysqli_query($link,$sql);
+    $Saturday = mysqli_fetch_array($result);
+	$count = 0;
+	
+	for ($x = 0; $x <= $limit; $x++) {
+		$count = $count + $Saturday[$x];
+	} 
+	$Saturday = $count;
+	
+	$sql = "SELECT total FROM transaction
   	        WHERE DATE_FORMAT(CAST(timedate AS date), '%W') = 'Sunday'
   	        ORDER BY DATE_FORMAT(CAST(timedate AS date), '%W') = 'Sunday'
-  	        LIMIT 3;";
+  	        LIMIT $limit;";
     $result = mysqli_query($link,$sql);
-    $row = mysqli_fetch_array($result);
-
+    $Sunday = mysqli_fetch_array($result);
+	$count = 0;
+	
+	for ($x = 0; $x <= $limit; $x++) {
+		$count = $count + $Sunday[$x];
+	} 
+	$Sunday = $count;
 ?>
 
 <!DOCTYPE html>
 <html>
   <head>
-    <link href='stylesheet.css' type='text/css' rel='stylesheet'>
+    <script src="Chart.js"></script>  
+    <link rel="stylesheet" type="text/css" href="stylesheet.css"/>
   </head>
+  
   <body>
     <div id='menu'>
       <a id="homeButton" href="adminpage.php">HOME</a>
@@ -33,17 +125,66 @@
     </div>
     <div id='content' class='content'>
       <h3>Summary of sales by day of the week</h3>
-      <form action="">
-        <input type="radio" name="dayofweek" class="weekOfDayRadio" value="Monday" checked>Monday
-        <input type="radio" name="dayofweek" class="weekOfDayRadio" value="Tuesday">Tuesday
-        <input type="radio" name="dayofweek" class="weekOfDayRadio" value="Wednesday">Wednesday
-        <input type="radio" name="dayofweek" class="weekOfDayRadio" value="Thursday">Thursday
-        <input type="radio" name="dayofweek" class="weekOfDayRadio" value="Friday">Friday
-        <input type="radio" name="dayofweek" class="weekOfDayRadio" value="Saturday">Saturday
-        <input type="radio" name="dayofweek" class="weekOfDayRadio" value="Sunday">Sunday
+      <form action="weekofday.php">
+        <input type="number" name="numberOfWeeks" min="1" max="52">
+        <input type="submit">
       </form>
-      <p>Here goes a graph</p>
-      <?php print_r($row); ?>
+      
     </div>
+    
+    <div class="graph">
+        <div>
+            <canvas id="canvas" height="450" width="600"></canvas>
+        </div>
+    </div>
+
+
+	<script>
+		
+		var lineChartData = {
+			labels : ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"],
+			datasets : [
+				{
+					label: "Income",
+					fillColor: "rgba(151,187,205,0.2)",
+					strokeColor: "rgba(151,187,205,1)",
+					pointColor: "rgba(151,187,205,1)",
+					pointStrokeColor: "#fff",
+					pointHighlightFill: "#fff",
+					pointHighlightStroke: "rgba(151,187,205,1)",
+					data : ["<?PHP echo $Monday;?>","<?PHP echo $Tuesday;?>","<?PHP echo $Wednesday;?>","<?PHP echo $Thursday;?>","<?PHP echo $Friday;?>","<?PHP echo $Monday;?>","<?PHP echo $Sunday;?>"]
+				}/*,
+				{
+					label: "Discount",
+					fillColor : "rgba(220,220,220,0.2)",
+					strokeColor : "rgba(220,220,220,1)",
+					pointColor : "rgba(220,220,220,1)",
+					pointStrokeColor : "#fff",
+					pointHighlightFill : "#fff",
+					pointHighlightStroke : "rgba(220,220,220,1)",
+					data : [1,2,3,4,5,6,7]
+				}*/
+			]
+
+		}
+
+	window.onload = function(){
+		var ctx = document.getElementById("canvas").getContext("2d");
+		window.myLine = new Chart(ctx).Line(lineChartData, {
+			responsive: true
+		});
+	}
+	</script>
+    <p>
+		<?PHP 
+			echo "<br><br><hr>Monday " . $limit . " Week Income Total: $" . $Monday . "<br>";
+			echo "Tuesday " . $limit . " Week Income Total: $" . $Tuesday . "<br>";
+			echo "Wednesday " . $limit . " Week Income Total: $" . $Wednesday . "<br>";
+			echo "Thursday " . $limit . " Week Income Total: $" . $Thursday . "<br>";
+			echo "Friday " . $limit . " Week Income Total: $" . $Friday . "<br>";
+			echo "Monday " . $limit . " Week Income Total: $" . $Monday . "<br>";
+			echo "Sunday " . $limit . " Week Income Total: $" . $Sunday . "<br>";
+        ?>
+	</p>
   </body>
 </html>
