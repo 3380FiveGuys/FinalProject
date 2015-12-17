70c40b1dff2f7554c073f4d2b64b55d44151e3a3
cmartinezvillar <cmartinezvillar90@gmail.com>
2015-12-16 20:58:31 -0600

    updated DDL.sql

--- database/DDL.sql
+++ database/DDL.sql
@@ -1,27 +1,5 @@
 /*** DDL.sql ***/
 
-CREATE TABLE brand(
-  supplier varchar(15) PRIMARY KEY
-) ENGINE = INNODB;
-
-CREATE TABLE food(
-  item_description varchar(25),
-  transaction_id bigint REFERENCES transaction ON DELETE CASCADE,
-  supplier varchar(15) REFERENCES brand ON DELETE CASCADE,
-  category varchar(20) REFERENCES food_category ON DELETE CASCADE,
-  unit_price decimal(6,2),
-  quantity integer,
-  price_total decimal(6,2),
-  item_discount decimal(6,2),
-  price decimal(6,2),
-  PRIMARY KEY(transaction_id)
-) ENGINE = INNODB;
-
-CREATE TABLE food_category(
-  department varchar(15),
-  category varchar(15) PRIMARY KEY
-) ENGINE = INNODB;
-
 CREATE TABLE transaction(
   transaction_id bigint REFERENCES operation_type,
   timedate datetime, /*e.g.: '12/11/2015 14:15' */
@@ -34,12 +12,6 @@ CREATE TABLE transaction(
   tax decimal(5,2),
   total decimal (7,2),
   PRIMARY KEY (transaction_id, timedate)
-) ENGINE = INNODB;
-
-CREATE TABLE food_transacion(
-  transaction_id bigint REFERENCES food,
-  timedate datetime REFERENCES transaction,
-  PRIMARY KEY(transaction_id, timedate)
 ) ENGINE = INNODB;
 
 CREATE TABLE customer_type(
